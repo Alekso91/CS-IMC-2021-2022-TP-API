8a3bca62da58c8cbaae6e887b01c9c499d98208d
Alex <alex.quilloux@student-cs.fr>
2022-01-14 15:57:33 +0100

    okok

---
+++ pypy.py
@@ -0,0 +1,69 @@
+import logging
+from py2neo import Graph
+from py2neo.bulk import create_nodes, create_relationships
+from py2neo.data import Node
+import os
+import pyodbc as pyodbc
+
+
+def main():
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = "jeanjean"
+    if not name:
+        try:
+            req_body = "ok"
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+    
+    server = os.environ["TPBDD_SERVER"]
+    database = os.environ["TPBDD_DB"]
+    username = os.environ["TPBDD_USERNAME"]
+    password = os.environ["TPBDD_PASSWORD"]
+    driver= '{ODBC Driver 17 for SQL Server}'
+
+    neo4j_server = os.environ["TPBDD_NEO4J_SERVER"]
+    neo4j_user = os.environ["TPBDD_NEO4J_USER"]
+    neo4j_password = os.environ["TPBDD_NEO4J_PASSWORD"]
+
+    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0 or len(neo4j_server)==0 or len(neo4j_user)==0 or len(neo4j_password)==0:
+        print("Worked")
+        
+    errorMessage = ""
+    dataString = ""
+    try:
+        logging.info("Test de connexion avec py2neo...")
+        graph = Graph(neo4j_server, auth=(neo4j_user, neo4j_password))
+        producers = graph.run("MATCH (n:Name)-[:PRODUCED]->(t:Title) WHERE t.primaryTitle CONTAINS 'Spider-Man' RETURN DISTINCT n.nconst, n.primaryName LIMIT 3")
+        for producer in producers:
+            dataString += f"CYPHER: nconst={producer['n.nconst']}, primaryName={producer['n.primaryName']}"
+
+        try:
+            logging.info("Test de connexion avec pyodbc...")
+            with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT TOP(3) tconst, primaryTitle, averageRating FROM [dbo].[tTitles] ORDER BY averageRating DESC")
+
+                rows = cursor.fetchall()
+                for row in rows:
+                    dataString += f"SQL: tconst={row[0]}, primaryTitle={row[1]}, averageRating={row[2]}\n"
+
+
+        except:
+            errorMessage = "Erreur de connexion a la base SQL"
+    except:
+        errorMessage = "Erreur de connexion a la base Neo4j"
+        
+    
+    if name:
+        nameMessage = f"Hello, {name}!\n"
+    else:
+        nameMessage = "Le parametre 'name' n'a pas ete fourni lors de l'appel.\n"
+    
+    if errorMessage != "":
+        print("Worked")
+
+    else:
+        print("Worked")
