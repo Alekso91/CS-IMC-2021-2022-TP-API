330018c3083c347263f60e01bb4837a48e07b24c
Alex <alex.quilloux@student-cs.fr>
2022-01-22 15:21:45 +0100

    query4 update

--- Query4/__init__.py
+++ Query4/__init__.py
@@ -1,24 +1,43 @@
 import logging
-
+import os
+import pyodbc as pyodbc
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    
+    server = os.environ["TPBDD_SERVER"]
+    database = os.environ["TPBDD_DB"]
+    username = os.environ["TPBDD_USERNAME"]
+    password = os.environ["TPBDD_PASSWORD"]
+    driver= '{ODBC Driver 17 for SQL Server}'
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0:
+        return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
+        
+    errorMessage = ""
+    dataString = ""
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    try:
+        logging.info("Test de connexion avec pyodbc...")
+        with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                            SELECT genre, AVG(runtimeMInutes) FROM [dbo].[tGenres]
+                            JOIN [dbo].[tTitles] ON [dbo].[tGenres].[tconst] = [dbo].[tTitles].[tconst]
+                            GROUP BY genre
+                            """)
+
+            rows = cursor.fetchall()
+            for row in rows:
+                dataString += f"SQL: genre={row[0]}, average={row[1]}\n"
+
+    except:
+        errorMessage = "Erreur de connexion a la base SQL"
+    
+    if errorMessage != "":
+        return func.HttpResponse(dataString + errorMessage, status_code=500)
+
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        return func.HttpResponse(dataString + " Connexion réussie a SQL!")
