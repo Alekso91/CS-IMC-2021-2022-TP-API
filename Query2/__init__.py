6984c688a5cc1f85e61ea4558a26c970546fb3b1
Alex <alex.quilloux@student-cs.fr>
2022-01-22 15:15:10 +0100

    query 2 update

--- Query2/__init__.py
+++ Query2/__init__.py
@@ -1,7 +1,4 @@
 import logging
-from py2neo import Graph
-from py2neo.bulk import create_nodes, create_relationships
-from py2neo.data import Node
 import os
 import pyodbc as pyodbc
 import azure.functions as func
@@ -25,45 +22,32 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     password = os.environ["TPBDD_PASSWORD"]
     driver= '{ODBC Driver 17 for SQL Server}'
 
-    neo4j_server = os.environ["TPBDD_NEO4J_SERVER"]
-    neo4j_user = os.environ["TPBDD_NEO4J_USER"]
-    neo4j_password = os.environ["TPBDD_NEO4J_PASSWORD"]
-
-    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0 or len(neo4j_server)==0 or len(neo4j_user)==0 or len(neo4j_password)==0:
+    if len(server)==0 or len(database)==0 or len(username)==0 or len(password)==0:
         return func.HttpResponse("Au moins une des variables d'environnement n'a pas été initialisée.", status_code=500)
         
     errorMessage = ""
     dataString = ""
+
     try:
-        logging.info("Test de connexion avec py2neo...")
-        graph = Graph(neo4j_server, auth=(neo4j_user, neo4j_password))
-        producers = graph.run("MATCH (n:Name) WHERE n.birthYear=1960 RETURN count(n)")
-        dataString += producers
+        logging.info("Test de connexion avec pyodbc...")
+        with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                            SELECT DISTINCT genre FROM [dbo].[tGenres]
+                            WHERE tconst IN
+                            ( SELECT tconst FROM [dbo].[tPrincipals] WHERE category='directed' OR category= 'acted in'
+                            GROUP BY nconst,tconst HAVING COUNT(DISTINCT(category))>1)
+                            """)
 
-        try:
-            logging.info("Test de connexion avec pyodbc...")
-            with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-                cursor = conn.cursor()
-                cursor.execute("""SELECT COUNT(*) FROM [dbo].[tNames] WHERE primaryName IS NOT NULL""")
+            rows = cursor.fetchall()
+            for row in rows:
+                dataString += f"SQL: genre={row[0]}\n"
 
-                rows = cursor.fetchone()
-                for row in rows:
-                    dataString += f"SQL: tconst={row[0]}, primaryTitle={row[1]}, averageRating={row[2]}\n"
-
-
-        except:
-            errorMessage = "Erreur de connexion a la base SQL"
     except:
-        errorMessage = "Erreur de connexion a la base Neo4j"
-        
-    
-    if name:
-        nameMessage = f"Hello, {name}!\n"
-    else:
-        nameMessage = "Le parametre 'name' n'a pas ete fourni lors de l'appel.\n"
+        errorMessage = "Erreur de connexion a la base SQL"
     
     if errorMessage != "":
-        return func.HttpResponse(dataString + nameMessage + errorMessage, status_code=500)
+        return func.HttpResponse(dataString + errorMessage, status_code=500)
 
     else:
-        return func.HttpResponse(dataString + nameMessage + " Connexions réussies a Neo4j et SQL!")
+        return func.HttpResponse(dataString + " Connexion réussie a SQL!")
